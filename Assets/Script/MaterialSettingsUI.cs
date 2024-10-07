4a99f583338d86550bcb3710a3663602e4b26446
hyunwook7120 <hyunwook7120@hanyang.ac.kr>
2024-10-07 21:06:24 +0900

    Feat: Add ForceHeat Case + Experiment Code

--- Assets/Script/MaterialSettingsUI.cs
+++ Assets/Script/MaterialSettingsUI.cs
@@ -13,15 +13,11 @@ public class MaterialSettingsUI : MonoBehaviour
         Test,
         Static,
         Dynamic,
-        Mix
+        Mix,
+        HeatForce
     }
-    // public enum HeatCase
-    // {
-    //     None,
-    //     Use
-    // }
+
     public ExperimentCase experiment;
-    // public HeatCase heat;
     public SG.SG_Material material; // SG_Material 스크립트가 연결된 게임 오브젝트
 
     public Button setMaxForceButton1;
@@ -31,9 +27,14 @@ public class MaterialSettingsUI : MonoBehaviour
     public Button setMaxForceButton5;
     public Button prevButton;
     public Button nextButton;
+    public Button LowButton;
+    public Button MediumButton;
+    public Button HighButton;
+    public Button ChangeButton;
     public CurveController CurveController;
     int count = -1;
     List<Vector2> testCase;
+    List<Vector3> testCase1;
     SG.SG_MaterialProperties materialProperties;
     AnimationCurve s1, s2,s3, s4, s5, i;
 
@@ -49,10 +50,19 @@ public class MaterialSettingsUI : MonoBehaviour
         i  = CurveController.impedence;
 
         SetMaxForce(0.0f);
-        SetMaxForceDist(0.0f);
+        SetMaxForceDist(0.06f);
+        materialProperties.forceRepsonse=s1;
 
         prevButton.onClick.AddListener(() => PrevForce());
         nextButton.onClick.AddListener(() => NextForce());
+        ChangeButton.onClick.AddListener(() => ChangeCase());
+
+        // string tempPath = Path.Combine(Application.dataPath, "Resources", "temperature.txt");
+        // StreamWriter swt = new StreamWriter(tempPath);
+        LowButton.onClick.AddListener(() => ChangeTemp(24f));
+        MediumButton.onClick.AddListener(() => ChangeTemp(32f));
+        HighButton.onClick.AddListener(() => ChangeTemp(40f));
+        ChangeTemp(32f);
         if (experiment == ExperimentCase.Test)
         {
             testCase = ShuffleTestCase(MakeTestCase1(1));
@@ -60,35 +70,47 @@ public class MaterialSettingsUI : MonoBehaviour
         else if (experiment == ExperimentCase.Static)
         {
             testCase = ShuffleTestCase(MakeTestCase1(5));
-            setMaxForceButton1.onClick.AddListener(() => SetMaxForce(0.0f));
-            setMaxForceButton2.onClick.AddListener(() => SetMaxForce(0.1f));
-            setMaxForceButton3.onClick.AddListener(() => SetMaxForce(0.3f));
-            setMaxForceButton4.onClick.AddListener(() => SetMaxForce(0.5f));
-            setMaxForceButton5.onClick.AddListener(() => SetMaxForce(1.0f));
-            SetMaxForceDist(0.06f);
-            materialProperties.forceRepsonse = i;
+            setMaxForceButton1.onClick.AddListener(() => {SetMaxForce(0.0f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton2.onClick.AddListener(() => {SetMaxForce(0.1f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton3.onClick.AddListener(() => {SetMaxForce(0.3f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton4.onClick.AddListener(() => {SetMaxForce(0.5f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton5.onClick.AddListener(() => {SetMaxForce(1.0f); materialProperties.forceRepsonse=i;});
         }
         else if (experiment == ExperimentCase.Dynamic)
         {
             testCase = ShuffleTestCase(MakeTestCase2(5));
-            setMaxForceButton1.onClick.AddListener(() => {SetMaxForce(0.0f); SetMaxForceDist(0.06f); materialProperties.forceRepsonse=s1;});
-            setMaxForceButton2.onClick.AddListener(() => {SetMaxForce(0.1f); SetMaxForceDist(0.06f); materialProperties.forceRepsonse=s2;});
-            setMaxForceButton3.onClick.AddListener(() => {SetMaxForce(0.3f); SetMaxForceDist(0.06f); materialProperties.forceRepsonse=s3;});
-            setMaxForceButton4.onClick.AddListener(() => {SetMaxForce(0.5f); SetMaxForceDist(0.06f); materialProperties.forceRepsonse=s4;});
-            setMaxForceButton5.onClick.AddListener(() => {SetMaxForce(1.0f); SetMaxForceDist(0.06f); materialProperties.forceRepsonse=s5;});
+            setMaxForceButton1.onClick.AddListener(() => {SetMaxForce(0.0f); materialProperties.forceRepsonse=s1;});
+            setMaxForceButton2.onClick.AddListener(() => {SetMaxForce(0.1f); materialProperties.forceRepsonse=s2;});
+            setMaxForceButton3.onClick.AddListener(() => {SetMaxForce(0.3f); materialProperties.forceRepsonse=s3;});
+            setMaxForceButton4.onClick.AddListener(() => {SetMaxForce(0.5f); materialProperties.forceRepsonse=s4;});
+            setMaxForceButton5.onClick.AddListener(() => {SetMaxForce(1.0f); materialProperties.forceRepsonse=s5;});
         }
         else if (experiment == ExperimentCase.Mix)
         {
             testCase = ShuffleTestCase(MakeTestCase3(5));
+        }
+        else if (experiment == ExperimentCase.HeatForce)
+        {
+            testCase1 = ShuffleTestCase1(MakeTestCase4(1));
         }
         
         string filePath = Path.Combine(Application.dataPath, "Resources", "Log.txt");
 
         using (StreamWriter sw = new StreamWriter(filePath))
         {
-            foreach (var item in testCase)
+            if (experiment != ExperimentCase.HeatForce)
             {
-                sw.WriteLine(item);
+                foreach (var item in testCase)
+                {
+                    sw.WriteLine(item);
+                }
+            }
+            else if (experiment == ExperimentCase.HeatForce)
+            {
+                foreach (var item in testCase1)
+                {
+                    sw.WriteLine(item);
+                }
             }
         }
 
@@ -121,41 +143,77 @@ public class MaterialSettingsUI : MonoBehaviour
 
         Debug.Log(count+1);
         SetMaxForce(testCase[count][0]);
-        SetMaxForceDist(testCase[count][1]);
     }
 
     void NextForce()
     {
         count++;
 
-        if (count >= testCase.Count)
+        if ((experiment != ExperimentCase.HeatForce) && (count >= testCase.Count))
+        {
+            Debug.LogError("Finish");
+        }
+        else if (count >= testCase1.Count)
         {
             Debug.LogError("Finish");
         }
         else
         {
             Debug.Log(count+1);
-            SetMaxForce(testCase[count][0]);
-            SetMaxForceDist(testCase[count][1]);
-            if (experiment == ExperimentCase.Dynamic)
+            
+            if (experiment == ExperimentCase.HeatForce)
             {
-                if (testCase[count][0] == 0.0f)
+                SetMaxForce(testCase1[count][0]);
+                ChangeTemp(testCase1[count][2]);
+                if (testCase1[count][1] == 0.0f)
+                {
+                    materialProperties.forceRepsonse = i;
+                }
+                else if (testCase1[count][1] == 1.0f)
                 {
                     materialProperties.forceRepsonse = s1;
                 }
-                else if (testCase[count][0] == 0.1f)
+                else if (testCase1[count][1] == 2.0f)
                 {
                     materialProperties.forceRepsonse = s2;
                 }
-                else if (testCase[count][0] == 0.3f)
+                else if (testCase1[count][1] == 3.0f)
                 {
                     materialProperties.forceRepsonse = s3;
                 }
-                else if (testCase[count][0] == 0.5f)
+                else if (testCase1[count][1] == 4.0f)
                 {
                     materialProperties.forceRepsonse = s4;
                 }
-                else if (testCase[count][0] == 1.0f)
+                else if (testCase1[count][1] == 5.0f)
+                {
+                    materialProperties.forceRepsonse = s5;
+                }
+            }
+            else
+            {
+                SetMaxForce(testCase[count][0]);
+                if (testCase[count][1] == 0.0f)
+                {
+                    materialProperties.forceRepsonse = i;
+                }
+                else if (testCase[count][1] == 1.0f)
+                {
+                    materialProperties.forceRepsonse = s1;
+                }
+                else if (testCase[count][1] == 2.0f)
+                {
+                    materialProperties.forceRepsonse = s2;
+                }
+                else if (testCase[count][1] == 3.0f)
+                {
+                    materialProperties.forceRepsonse = s3;
+                }
+                else if (testCase[count][1] == 4.0f)
+                {
+                    materialProperties.forceRepsonse = s4;
+                }
+                else if (testCase[count][1] == 5.0f)
                 {
                     materialProperties.forceRepsonse = s5;
                 }
@@ -163,20 +221,48 @@ public class MaterialSettingsUI : MonoBehaviour
         }
     }
 
+    void ChangeCase()
+    {
+        if (experiment == ExperimentCase.Static)
+        {
+            setMaxForceButton1.onClick.AddListener(() => {SetMaxForce(0.0f); materialProperties.forceRepsonse=s1;});
+            setMaxForceButton2.onClick.AddListener(() => {SetMaxForce(0.1f); materialProperties.forceRepsonse=s2;});
+            setMaxForceButton3.onClick.AddListener(() => {SetMaxForce(0.3f); materialProperties.forceRepsonse=s3;});
+            setMaxForceButton4.onClick.AddListener(() => {SetMaxForce(0.5f); materialProperties.forceRepsonse=s4;});
+            setMaxForceButton5.onClick.AddListener(() => {SetMaxForce(1.0f); materialProperties.forceRepsonse=s5;});
+            Debug.Log("D");
+            experiment = ExperimentCase.Dynamic;
+        }
+        else if (experiment == ExperimentCase.Dynamic)
+        {
+            setMaxForceButton1.onClick.AddListener(() => {SetMaxForce(0.0f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton2.onClick.AddListener(() => {SetMaxForce(0.1f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton3.onClick.AddListener(() => {SetMaxForce(0.3f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton4.onClick.AddListener(() => {SetMaxForce(0.5f); materialProperties.forceRepsonse=i;});
+            setMaxForceButton5.onClick.AddListener(() => {SetMaxForce(1.0f); materialProperties.forceRepsonse=i;});
+            Debug.Log("S");
+            experiment = ExperimentCase.Static;
+        }
+    }
+
+    void ChangeTemp(float temp)
+    {       
+        string filePath = Path.Combine(Application.dataPath, "Resources", "temperature.txt");
+
+        StreamWriter swt = new StreamWriter(filePath);
+        swt.WriteLine(temp);
+        swt.Close();
+    }
+
     List<Vector2> MakeTestCase1(int repeat)
     {
         List<Vector2> originalTestCase = new List<Vector2>
         {
-            new Vector2(0.0f, 0.06f),
-            new Vector2(0.1f, 0.06f),
-            new Vector2(0.3f, 0.06f),
-            new Vector2(0.5f, 0.06f),
-            new Vector2(1.0f, 0.06f),
-            // new Vector2(0.0f, 0.0f),
-            // new Vector2(0.1f, 0.0f),
-            // new Vector2(0.3f, 0.0f),
-            // new Vector2(0.5f, 0.0f),  
-            // new Vector2(1.0f, 0.0f),
+            new Vector2(0.0f, 0.0f),
+            new Vector2(0.1f, 0.0f),
+            new Vector2(0.3f, 0.0f),
+            new Vector2(0.5f, 0.0f),
+            new Vector2(1.0f, 0.0f),
         };
 
         List<Vector2> testCase = new List<Vector2>();
@@ -193,16 +279,11 @@ public class MaterialSettingsUI : MonoBehaviour
     {
         List<Vector2> originalTestCase = new List<Vector2>
         {
-            new Vector2(0.0f, 0.06f),
-            new Vector2(0.1f, 0.06f),
-            new Vector2(0.3f, 0.06f),
-            new Vector2(0.5f, 0.06f),
-            new Vector2(1.0f, 0.06f),
-            // new Vector2(0.0f, 0.06f),
-            // new Vector2(0.1f, 0.06f),
-            // new Vector2(0.3f, 0.06f),
-            // new Vector2(0.5f, 0.06f),
-            // new Vector2(1.0f, 0.06f)
+            new Vector2(0.0f, 1.0f),
+            new Vector2(0.1f, 2.0f),
+            new Vector2(0.3f, 3.0f),
+            new Vector2(0.5f, 4.0f),
+            new Vector2(1.0f, 5.0f)
         };
 
         List<Vector2> testCase = new List<Vector2>();
@@ -224,13 +305,54 @@ public class MaterialSettingsUI : MonoBehaviour
             new Vector2(0.3f, 0.0f),
             new Vector2(0.5f, 0.0f),
             new Vector2(1.0f, 0.0f),
-            new Vector2(0.0f, 0.06f),
-            new Vector2(0.1f, 0.06f),
-            new Vector2(0.3f, 0.06f),
-            new Vector2(0.5f, 0.06f),
-            new Vector2(1.0f, 0.06f)
+            new Vector2(0.0f, 1.0f),
+            new Vector2(0.1f, 2.0f),
+            new Vector2(0.3f, 3.0f),
+            new Vector2(0.5f, 4.0f),
+            new Vector2(1.0f, 5.0f)
         };
         List<Vector2> testCase = new List<Vector2>();
+
+        for (int i = 0; i < repeat; i++)
+        {
+            testCase.AddRange(originalTestCase);
+        }
+
+        return testCase;
+    }
+    List<Vector3> MakeTestCase4(int repeat)
+    {
+        List<Vector3> originalTestCase = new List<Vector3>
+        {
+            new Vector3(0.0f, 0.0f, 24f),
+            new Vector3(0.1f, 0.0f, 24f),
+            new Vector3(0.3f, 0.0f, 24f),
+            new Vector3(0.5f, 0.0f, 24f),
+            new Vector3(1.0f, 0.0f, 24f),
+            new Vector3(0.1f, 2.0f, 24f),
+            new Vector3(0.3f, 3.0f, 24f),
+            new Vector3(0.5f, 4.0f, 24f),
+            new Vector3(1.0f, 5.0f, 24f),
+            new Vector3(0.0f, 0.0f, 32f),
+            new Vector3(0.1f, 0.0f, 32f),
+            new Vector3(0.3f, 0.0f, 32f),
+            new Vector3(0.5f, 0.0f, 32f),
+            new Vector3(1.0f, 0.0f, 32f),
+            new Vector3(0.1f, 2.0f, 32f),
+            new Vector3(0.3f, 3.0f, 32f),
+            new Vector3(0.5f, 4.0f, 32f),
+            new Vector3(1.0f, 5.0f, 32f),
+            new Vector3(0.0f, 0.0f, 40f),
+            new Vector3(0.1f, 0.0f, 40f),
+            new Vector3(0.3f, 0.0f, 40f),
+            new Vector3(0.5f, 0.0f, 40f),
+            new Vector3(1.0f, 0.0f, 40f),
+            new Vector3(0.1f, 2.0f, 40f),
+            new Vector3(0.3f, 3.0f, 40f),
+            new Vector3(0.5f, 4.0f, 40f),
+            new Vector3(1.0f, 5.0f, 40f)
+        };
+        List<Vector3> testCase = new List<Vector3>();
 
         for (int i = 0; i < repeat; i++)
         {
@@ -255,4 +377,18 @@ public class MaterialSettingsUI : MonoBehaviour
         return testCase;
     }
 
+        public static List<Vector3> ShuffleTestCase1(List<Vector3> testCase)
+    {
+        var rng = new System.Random();
+        int n = testCase.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            Vector3 value = testCase[k];
+            testCase[k] = testCase[n];
+            testCase[n] = value;
+        }
+        return testCase;
+    }
 }
